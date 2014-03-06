95921f2fe1a2005622388da084a0f44f97c7d092
Halit Anil Donmez <halitanil.donmez@gmail.com>
2014-03-06 11:59:59 +0100

    added styles to the tables.
    put the start and end times on the same table.
    
    added css class attribute to the overall table

--- js/view/meetingsView.js
+++ js/view/meetingsView.js
@@ -1,5 +1,10 @@
 /**
  * Created by sandstroh on 3/3/14.
+    <table class="table table-bordered" id="meeting-table">
+        <tr>
+            <td class="active">Race</td>
+        </tr>
+    </table>
  */
 var MeetingsView = function(container, model) {
 
@@ -12,15 +17,25 @@ var MeetingsView = function(container, model) {
 
         console.log('meetingsView.update()');
 
-        for (var i = 0; i < model.days.length; i++) {
+        var meetingTable = $('<table>');
+        meetingTable.addClass("table table-bordered");
+        meetingTable.attr("id","meeting-table");
+
+        for (var i = 0; i < model.days.length; i++)
+        {
+            var trTag = $("<tr>");
+            var column = $("<td>");
+            column.addClass("active");
+
             var dayDiv = $('<div>');
             var dayView = new DayView(dayDiv, model, model.days[i]);
             var dayController = new DayController(dayView, model, model.days[i]);
-            this.dayViewsContainer.append(dayDiv);
+
+            column.append(dayDiv);
+            trTag.append(column);
+            meetingTable.append(trTag);
+            this.dayViewsContainer.append(meetingTable);
         }
-
     }
-
     this.update();
-
 }
