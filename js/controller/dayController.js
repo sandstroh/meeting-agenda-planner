9055a9599ba64095eae8ab4f330af163dbaf11e2
Sandro Affentranger <asandros@kth.se>
2014-03-06 15:08:23 +0100

    Started implementing drag n drop feature

--- js/controller/dayController.js
+++ js/controller/dayController.js
@@ -3,20 +3,37 @@
  */
 var DayController = function(view, model, day) {
 
-    // TODO: drag n drop doesn't work yet
-
-//    view.on('ondragover', onDragOver);
-//    view.activitiesDiv.on('ondragover', onDragOver);
-//    view.activitiesDiv.on('ondrop', onDrop);
-
-    var onDragOver = function(event) {
+    // onDragOver: we need to catch this event and stop the propagation
+    // otherwise the 'drop' event won't be fired
+    view.activitiesDiv.on('dragover', this, function(event) {
         event.preventDefault();
-    }
+        event.stopPropagation();
+    });
 
-    var onDrop = function(event) {
+    // onDragEnter:
+    view.activitiesDiv.on('dragenter', this, function(event) {
+        console.log('dragenter()');
+        event.target.classList.add('over');
+    });
+
+    // onDragLeave:
+    view.activitiesDiv.on('dragleave', this, function(event) {
+        console.log('dragleave()');
         event.preventDefault();
-        var data = event.dataTransfer.getData("Text");
-        event.target.appendChild(document.getElementById(data))
-    }
+        event.stopPropagation();
+        event.target.classList.remove('over');
+    });
+
+    // onDrop:
+    view.activitiesDiv.on('drop', this, function(event) {
+        console.log('drop()');
+        event.preventDefault();
+        event.stopPropagation();
+        event.target.classList.remove('over');
+        // TODO: retrieve activity from event and add it to the day
+        // TODO: in the model
+//        var data = event.originalEvent.dataTransfer.getData("Text");
+//        view.activitiesDiv.append(data);
+    });
 
 }
